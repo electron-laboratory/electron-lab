f0d61266582a9145f9351d525dc02d30b806822e
xiefengnian.xfn <xiefengnian.xfn@antgroup.com>
2021-09-01 17:13:21 +0800

    feat: init

---
+++ packages/electron-lab/config/main.webpack.config.js
@@ -0,0 +1,27 @@
+const { resolve } = require('path');
+
+module.exports = {
+  entry: resolve(process.cwd(), 'src/main/index.ts'),
+  output: {
+    path: resolve(process.cwd(), '.webpack/main'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+  devtool: 'source-map',
+  target: 'electron-main',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)$/,
+        use: {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [require.resolve('@babel/preset-typescript')],
+          },
+        },
+      },
+    ],
+  },
+};
