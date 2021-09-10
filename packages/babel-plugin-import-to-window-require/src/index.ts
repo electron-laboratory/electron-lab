6b585fd3eb5551d4a36922ad31ebfcbc84a16633
xiefengnian.xfn <xiefengnian.xfn@antgroup.com>
2021-09-10 18:03:46 +0800

    feat(babel-plugin-import-to-window-require): Publish 1.0.0

---
+++ packages/babel-plugin-import-to-window-require/src/index.ts
@@ -0,0 +1,70 @@
+import { NodePath } from '@babel/traverse';
+import { ImportDeclaration } from '@babel/types';
+
+type Opts = {
+  packages: string[];
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function({ types: t }) {
+  return {
+    visitor: {
+      ImportDeclaration: {
+        exit(path: NodePath<ImportDeclaration>, { opts }: { opts: Opts }): void {
+          const { packages } = opts;
+
+          const { specifiers, source } = path.node;
+
+          if (!specifiers.length || !packages.includes(source.value)) {
+            return;
+          }
+          const importSpecifiers = [];
+          let defaultImport;
+          specifiers.forEach(spec => {
+            if (t.isImportDefaultSpecifier(spec)) {
+              const {
+                local: { name },
+              } = spec;
+              defaultImport = t.variableDeclaration('const', [
+                t.variableDeclarator(
+                  t.identifier(name),
+                  t.callExpression(
+                    t.memberExpression(t.identifier('window'), t.identifier('require')),
+                    [t.stringLiteral(source.value)],
+                  ),
+                ),
+              ]);
+            } else if (t.isImportSpecifier(spec)) {
+              importSpecifiers.push(spec);
+            }
+          });
+          const extractImport =
+            importSpecifiers.length > 0
+              ? t.variableDeclaration('const', [
+                  t.variableDeclarator(
+                    t.objectPattern([
+                      ...importSpecifiers.map(spec => {
+                        const {
+                          imported: { name: importedName },
+                          local: { name: localName },
+                        } = spec;
+                        return t.objectProperty(
+                          t.identifier(importedName),
+                          t.identifier(localName),
+                        );
+                      }),
+                    ]),
+                    t.callExpression(
+                      t.memberExpression(t.identifier('window'), t.identifier('require')),
+                      [t.stringLiteral(source.value)],
+                    ),
+                  ),
+                ])
+              : undefined;
+
+          path.replaceWithMultiple([defaultImport, extractImport].filter(Boolean));
+        },
+      },
+    },
+  };
+}
